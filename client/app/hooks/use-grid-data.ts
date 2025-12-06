28c898a8df4f143067800deef711e27f74745125
Karol Jażdrzyk <karol1404@vp.pl>
2025-12-06 21:29:12 +0100

    Create hook for fetching all grid data at once

---
+++ client/app/hooks/use-grid-data.ts
@@ -0,0 +1,29 @@
+import { useDzialy } from '../queries';
+import { useRozdzialy } from '../queries';
+import { useParagrafy } from '../queries';
+import { useGrupyWydatkow } from '../queries';
+import { useCzesciBudzetowe } from '../queries';
+
+export const useGridData = () => {
+  const { data: dzialy, isLoading: isLoadingDzialy } = useDzialy();
+  const { data: rozdzialy, isLoading: isLoadingRozdzialy } = useRozdzialy();
+  const { data: paragrafy, isLoading: isLoadingParagrafy } = useParagrafy();
+  const { data: grupyWydatkow, isLoading: isLoadingGrupyWydatkow } =
+    useGrupyWydatkow();
+  const { data: czesciBudzetowe, isLoading: isLoadingCzesciBudzetowe } =
+    useCzesciBudzetowe();
+
+  return {
+    dzialy,
+    rozdzialy,
+    paragrafy,
+    grupyWydatkow,
+    czesciBudzetowe,
+    isLoading:
+      isLoadingDzialy ||
+      isLoadingRozdzialy ||
+      isLoadingParagrafy ||
+      isLoadingGrupyWydatkow ||
+      isLoadingCzesciBudzetowe,
+  };
+};
--- client/app/routes/edit.tsx
+++ client/app/routes/edit.tsx
@@ -1,8 +1,29 @@
+import { useGridData } from '~/hooks/use-grid-data';
+
 export function meta() {
   return [{ title: 'Edit' }];
 }
 
 export default function Edit() {
+  const {
+    dzialy,
+    rozdzialy,
+    paragrafy,
+    grupyWydatkow,
+    czesciBudzetowe,
+    isLoading,
+  } = useGridData();
+
+  if (!isLoading) {
+    console.log({
+      dzialy,
+      rozdzialy,
+      paragrafy,
+      grupyWydatkow,
+      czesciBudzetowe,
+    });
+  }
+
   return (
     <div>
       <h2>Edit Screen</h2>
