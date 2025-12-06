ed91ac2c4b0952cb6d3d0e46d526d512368b3679
Karol Jażdrzyk <karol1404@vp.pl>
2025-12-06 22:38:45 +0100

    Add buttons to edit page

--- client/app/routes/edit.tsx
+++ client/app/routes/edit.tsx
@@ -1,9 +1,11 @@
 import { useGridData } from '~/hooks/use-grid-data';
 import { useForm } from '@tanstack/react-form';
 import { budgetDocumentSchema } from '~/schema';
+import { Button } from '~/components/ui/button';
+import { Download, FileText, ListPlusIcon } from 'lucide-react';
 
 export function meta() {
-  return [{ title: 'Edit' }];
+  return [{ title: 'Budżety' }];
 }
 
 export default function Edit() {
@@ -25,7 +27,23 @@ export default function Edit() {
 
   return (
     <div>
-      <h2>Edit Screen</h2>
+      <div className='flex justify-between items-center'>
+        <h1 className='font-semibold text-2xl py-6'>Budżety</h1>
+        <div className='flex gap-[15px]'>
+          <Button onClick={() => form.submit()} size='sm' variant='secondary'>
+            <FileText />
+            <span>Generuj pismo</span>
+          </Button>
+          <Button onClick={() => form.submit()} size='sm' variant='secondary'>
+            <Download />
+            Export
+          </Button>
+          <Button onClick={() => form.submit()} size='sm' variant='secondary'>
+            <ListPlusIcon />
+            Dodaj nowy wiersz
+          </Button>
+        </div>
+      </div>
       <p>This is the edit screen.</p>
     </div>
   );
