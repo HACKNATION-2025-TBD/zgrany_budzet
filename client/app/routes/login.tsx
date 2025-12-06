32da4674e0ae2d90f7a6bdc50c0d9934af306a1e
Karol Jażdrzyk <karol1404@vp.pl>
2025-12-06 18:02:55 +0100

    Add button component

--- client/app/routes/login.tsx
+++ client/app/routes/login.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router';
+import MC from '@/assets/mc.png';
+import { Button } from '@/components/ui/button';
 
 export function meta() {
   return [{ title: 'Login' }];
@@ -7,12 +9,18 @@ export function meta() {
 export default function Login() {
   return (
     <div>
-      <h2>Login Screen</h2>
-      <p>Login to access your dashboard and edit screen.</p>
-      <nav>
-        <Link to='/dashboard'>Go to Dashboard</Link>
-        <br />
-        <Link to='/edit'>Go to Edit Screen</Link>
+      <header className='p-4 border-b border-gov-gray-500'>
+        <img src={MC} alt='Ministerstwo Cyfryzacji' width={150} />
+      </header>
+      <h1 className='font-semibold text-2xl py-6'>Zaloguj się do usługi</h1>
+      <p className='text-gov-gray-500'>Wybierz rolę:</p>
+      <nav className='flex flex-col gap-4 py-4'>
+        <Link to='/dashboard'>
+          <Button>Kierownictwo</Button>
+        </Link>
+        <Link to='/edit'>
+          <Button>Komórki organizacyjne</Button>
+        </Link>
       </nav>
     </div>
   );
