b23e06570477bb3ece81c6599a8cdcea4e709fd4
Karol Jażdrzyk <karol1404@vp.pl>
2025-12-06 18:05:46 +0100

    Fix colors

--- client/app/routes/login.tsx
+++ client/app/routes/login.tsx
@@ -9,11 +9,11 @@ export function meta() {
 export default function Login() {
   return (
     <div>
-      <header className='p-4 border-b border-gov-gray-500'>
+      <header className='p-4 border-b border-border'>
         <img src={MC} alt='Ministerstwo Cyfryzacji' width={150} />
       </header>
       <h1 className='font-semibold text-2xl py-6'>Zaloguj się do usługi</h1>
-      <p className='text-gov-gray-500'>Wybierz rolę:</p>
+      <p className='text-foreground'>Wybierz rolę:</p>
       <nav className='flex flex-col gap-4 py-4'>
         <Link to='/dashboard'>
           <Button>Kierownictwo</Button>
