4434dffffa1f225a0dbcbc720f3ec2c7005f830a
Karol Jażdrzyk <karol1404@vp.pl>
2025-12-07 01:03:45 +0100

    Add missing buttons

--- client/app/hooks/use-user-mock.ts
+++ client/app/hooks/use-user-mock.ts
@@ -3,20 +3,20 @@ import { useLocalStorageValue } from './use-local-storage-value';
 const userMockData = {
   kierownictwo: {
     name: 'Joanna Kowalska',
-    role: 'Kierownictwo',
+    role: 'kierownictwo',
     email: 'joakow@mc.gov.pl',
   },
   bbf: {
     name: 'Beata Fąk',
-    role: 'Biuro Budżetowo-Finansowe',
+    role: 'bbf',
     email: 'annnow@mc.gov.pl',
   },
   ko: {
     name: 'Karolina Olycz',
-    role: 'Komórki organizacyjne',
+    role: 'ko',
     email: 'karoly@mc.gov.pl',
   },
-} as Record<UserType, { name: string; role: string; email: string }>;
+} as Record<UserType, { name: string; role: UserType; email: string }>;
 
 export type UserType = 'kierownictwo' | 'bbf' | 'ko';
 
--- client/app/routes/edit.tsx
+++ client/app/routes/edit.tsx
@@ -1,14 +1,22 @@
 import { budgetDocumentRowSchema, budgetDocumentSchema } from '~/schema';
 import { Button } from '~/components/ui/button';
-import { Download, FileText, ListPlusIcon } from 'lucide-react';
+import {
+  Download,
+  FileLockIcon,
+  FileText,
+  History,
+  ListPlusIcon,
+} from 'lucide-react';
 import BudgetGrid from '~/components/budget-grid';
 import { useState } from 'react';
+import { useUserMock } from '~/hooks/use-user-mock';
 
 export function meta() {
   return [{ title: 'Budżety' }];
 }
 
 export default function Edit() {
+  const { user } = useUserMock();
   const [budgetDocument, setBudgetDocument] = useState(
     budgetDocumentSchema.parse([])
   );
@@ -18,14 +26,30 @@ export default function Edit() {
       <div className='flex justify-between items-center'>
         <h1 className='font-semibold text-2xl py-6'>Budżety</h1>
         <div className='flex gap-[15px]'>
-          <Button onClick={() => {}} size='sm' variant='secondary'>
-            <FileText />
-            <span>Generuj pismo</span>
-          </Button>
-          <Button onClick={() => {}} size='sm' variant='secondary'>
-            <Download />
-            Export
-          </Button>
+          {user.role !== 'ko' && (
+            <Button onClick={() => {}} size='sm' variant='secondary'>
+              <FileLockIcon />
+              <span>Zablokuj</span>
+            </Button>
+          )}
+          {user.role !== 'ko' && (
+            <Button onClick={() => {}} size='sm' variant='secondary'>
+              <History />
+              <span>Wersje</span>
+            </Button>
+          )}
+          {user.role !== 'ko' && (
+            <Button onClick={() => {}} size='sm' variant='secondary'>
+              <FileText />
+              <span>Generuj pismo</span>
+            </Button>
+          )}
+          {user.role !== 'ko' && (
+            <Button onClick={() => {}} size='sm' variant='secondary'>
+              <Download />
+              Export
+            </Button>
+          )}
           <Button
             onClick={() => {
               setBudgetDocument((prev) => [
